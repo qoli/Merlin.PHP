034bfc7bde6d926d0db7a2e5b0aabc80052c2db4
Qoli Wong <llqoli@gmail.com>
2016-06-21 00:18:00 +0800

    md5 check

--- bin/autoupdate/build.sh
+++ bin/autoupdate/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-MD5=$(echo -n date '+%A %W %Y %X' | md5 -r | awk '{ print $1 }')
-echo $MD5 > ./md5
+MD5=$(date '+%A %W %Y %X' | md5 -r | awk '{ print $1 }')
+echo $MD5 > md5
 tar -cvf merlin-php.tar.gz -C ./../../../ --exclude=Merlin.PHP/bin/zip/ --exclude=Merlin.PHP/.* --exclude=Merlin.PHP/sftp-config.json  --exclude=Merlin.PHP/md5 Merlin.PHP
 mv merlin-php.tar.gz ../zip/
 # md5sum='md5 -r'
 # MD5=$(md5 -r ../zip/merlin-php.tar.gz | awk '{ print $1 }')
+cat md5
