1a9b37843bf819407ac41d81d1e8cbdff1c44497
Qoli Wong <llqoli@gmail.com>
2016-06-20 21:50:53 +0800

    R

--- assets/news.js
+++ assets/news.js
@@ -14,25 +14,42 @@ jQuery(document).ready(function($) {
       update_news(url);
     }
 
+    console.log(getUrlParameter('a'));
+
   }
 
   // https://raw.githubusercontent.com/qoli/Merlin.PHP/master/article/help.md
 
 });
 
+  var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+
+      if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined ? true : sParameterName[1];
+      }
+    }
+  };
+
 function update_news(url) {
 
   uArray = url.split( '/' );
   $articletitle.text(uArray[uArray.length -1]);
-  console.log("run");
+  // console.log("run");
   $.ajax({
     url: url,
     dataType: "text",
     beforeSend: function(){
-      LoadingBox(true,'處理中：');
+      LoadingBox(true,'加載內容……');
     },
     success: function(data) {
-      console.log(data);
+      // console.log(data);
       $NewsBox.html(markdown.toHTML(data));
     },
     error: function() {
