6f01ac0250d739609ae89957eb0968cd1a76ffa5
Qoli Wong <llqoli@gmail.com>
2016-06-20 23:32:54 +0800

    autoupdate

--- bin/autoupdate/build.sh
+++ bin/autoupdate/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-tar -cvf merlin-php.tar.gz -C ./../../../ --exclude=Merlin.PHP/bin --exclude=Merlin.PHP/.* Merlin.PHP
+tar -cvf merlin-php.tar.gz -C ./../../../ --exclude=Merlin.PHP/bin/zip/ --exclude=Merlin.PHP/.* Merlin.PHP
+mv merlin-php.tar.gz ../zip/
 # md5sum='md5 -r'
-MD5=$(md5 -r merlin-php.tar.gz | awk '{ print $1 }')
-echo $MD5 >>./md5
+MD5=$(md5 -r ../zip/merlin-php.tar.gz | awk '{ print $1 }')
+echo $MD5 > ./md5
--- bin/autoupdate/update.sh
+++ bin/autoupdate/update.sh
@@ -1,6 +1,42 @@
 #!/bin/sh
-cd /tmp
-wget https://github.com/qoli/Merlin.PHP/archive/master.zip --no-check-certificate
-unzip master.zip
-cd Merlin.PHP-master
-cp * /opt/share/www/ -R
+
+way=0
+
+cd ../../
+echo
+remote_md5=$(curl https://raw.githubusercontent.com/qoli/Merlin.PHP/master/bin/autoupdate/md5)
+local_md5=$(cat md5)
+echo
+echo "local :" $local_md5
+echo "remote:" $remote_md5
+echo
+
+if [ "$remote_md5" != "$local_md5" ]; then
+	way='need'
+fi
+
+case $way in
+  0)
+    echo 'The version is up-to-date.'
+    ;;
+  need)
+  	echo 'need update.'
+  	echo
+
+		rm -rf /tmp/m_update
+		rm -rf /opt/share/www/new/
+		mkdir /tmp/m_update
+		cd /tmp/m_update
+		wget https://github.com/qoli/Merlin.PHP/raw/master/bin/autoupdate/merlin-php.tar.gz --no-check-certificate
+		echo 'extract...'
+  	echo
+		tar xvf merlin-php.tar.gz
+		echo 'copying...'
+  	echo
+		mkdir /opt/share/www/new/ -p
+		cd /tmp/m_update/Merlin.PHP/
+		cp * /opt/share/www/new/ -R
+		echo 'done'
+  	echo
+    ;;
+esac
