fb819e53947ef69e634fda56f171657182f21862
Qoli Wong <llqoli@gmail.com>
2016-07-27 11:47:25 +0800

    刪除多餘命令（即時網速腳本）

--- bin/script/netspeed.sh
+++ bin/script/netspeed.sh
@@ -8,11 +8,6 @@ sleep 1
 up_time2=`cat /sys/class/net/$1/statistics/tx_bytes`
 down_time2=`cat /sys/class/net/$1/statistics/rx_bytes`
 
-up_time1=${up_time1##bytes:}
-up_time2=${up_time2##bytes:}
-down_time1=${down_time1##bytes:}
-down_time2=${down_time2##bytes:}
-
 up_time=`expr $up_time2 - $up_time1`
 down_time=`expr $down_time2 - $down_time1`
 up_time=`expr $up_time / 1024`
