a1394e7cd5dc3b79af1398cc05b995c736f15474
Qoli Wong <llqoli@gmail.com>
2016-07-28 00:53:38 +0800

    添加通信失敗的反饋

--- bin/autoupdate/check.sh
+++ bin/autoupdate/check.sh
@@ -15,18 +15,25 @@ if [ "$remote_md5" != "$local_md5" ]; then
 	way='need'
 fi
 
+if [ "$remote_md5" == "" ]; then
+	way='empty'
+fi
+
 case $way in
   0)
     echo 'The version is up-to-date.'
+		echo
+		cd bin/autoupdate/
+		echo $local_md5 '->' $(cat md5)
     ;;
   need)
   	echo 'need update.'
   	echo '請點擊上方「實行更新」'
     ;;
+  empty)
+  	echo 'github.com timeout'
+  	echo '與 github.com 的通信失敗'
+    ;;
 esac
-
-echo
-cd bin/autoupdate/
-echo $local_md5 '->' $(cat md5)
 echo
 date
