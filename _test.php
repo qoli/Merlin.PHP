2ce60a54a75f4d903ce61825844bd4d422018dca
Qoli Wong <llqoli@gmail.com>
2016-08-03 12:01:47 +0800

    dashboard

--- _test.php
+++ _test.php
@@ -6,11 +6,31 @@ require_once 'library/Requests.php';
 require_once 'library/AppFunction.php';
 Requests::register_autoloader();
 
-exec("ls /mnt/",$o);
+$headers = array('Content-Type' => 'application/javascript');
+// $options = array('login_authorization' => base64_encode("admin:2202832"));
+// $request = Requests::post('http://192.168.1.1/login.cgi', $headers, $options);
+// $headers = array('Cookie' => $request->headers['set-cookie']);
+$o = Requests::get('http://192.168.1.1/update_clients.asp',$headers);
+// $o = Requests::get('http://192.168.1.1/Advanced_WAN_Content.asp',$headers);
 
-dump($o);
+$networkmap = $o->body;
 
-foreach ($o as $key => $value) {
-	exec("ls /mnt/".$value,$k);
-	dump($k,$value);
+if (strpos($networkmap,'<HTML><HEAD><script>top.location.href') !== false) {
+	echo "登入失敗";
+	exit;
 }
+
+$networkmap = str_replace('originDataTmp = originData;','',$networkmap);
+// $networkmap = str_replace('originData = ','',$networkmap);
+$networkmap = str_replace("networkmap_fullscan = '0';",'',$networkmap);
+$networkmap = str_replace('if(networkmap_fullscan == 1) genClientList();','',$networkmap);
+
+?>
+
+<script type="text/javascript">
+<?=$networkmap;?>
+console.log(originData);
+</script>
+<body>
+	<?=$networkmap;?>
+</body>
