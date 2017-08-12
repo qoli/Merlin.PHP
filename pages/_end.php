da80a3c7d73d398af0eddd69afb5dff7a118c179
Qoli Wong <llqoli@gmail.com>
2017-08-12 13:39:25 +0800

    調整 UI

--- pages/_end.php
+++ pages/_end.php
@@ -3,7 +3,7 @@
 <script src="assets/underscore-min.js"></script>
 <script src="assets/logs.js"></script>
 <script src="assets/ui.js"></script>
-<script src="assets/app.js"></script>
+<script src="assets/app.js?1"></script>
 <script src="assets/standalone.js"></script>
 
 <script>if (typeof module === 'object') {window.module = module; module = undefined;}</script>
